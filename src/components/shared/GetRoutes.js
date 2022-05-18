0a307931d7c560359f530514ac5eb2dde9154450
shahnewaz171 <shahnewaz601@gmail.com>
2022-05-18 21:59:00 +0600

    setup routes

---
+++ src/components/shared/GetRoutes.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { Outlet, useRoutes } from 'react-router-dom';
+import TableInfo from '../TableInfo/TableInfo';
+
+const GetRoutes = () => {
+    
+    let element = useRoutes([
+        {
+            path: "/",
+            element: <Outlet />,
+            children: [
+                { index: true, element: <TableInfo /> },
+                { path: "create_form", element: <h1>Create form</h1> },
+                { path: "update_form", element: <h1>Update form</h1> },
+            ],
+        },
+        { path: "*", element: <h1>Not found</h1> },
+    ]);
+
+    return element;
+}
+
+export default GetRoutes;
