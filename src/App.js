301fa9e8f888c5b3f621dc3567484945308f93c6
shahnewaz171 <shahnewaz601@gmail.com>
2022-05-19 02:50:47 +0600

    added sortedfunctionalitie in the table

--- src/App.js
+++ src/App.js
@@ -1,15 +1,26 @@
-import React from 'react';
+import axios from 'axios';
+import React, { createContext, useEffect, useState } from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import './App.css';
 import GetRoutes from './components/shared/GetRoutes';
 import Navbar from './components/shared/Navbar/Navbar';
 
+export const GlobalContext = createContext();
+
 const App = () => {
+  const [tableData, setTableData] = useState([]);
  
   return (
     <BrowserRouter>
-      <Navbar />
-      <GetRoutes />
+      <GlobalContext.Provider
+        value={{ 
+          tableData,
+          setTableData
+         }}
+      >
+        <Navbar />
+        <GetRoutes />
+      </GlobalContext.Provider>
     </BrowserRouter>
   );
 }
--- src/components/TableInfo/TableInfo.js
+++ src/components/TableInfo/TableInfo.js
@@ -1,75 +1,111 @@
-import { Box, Paper, Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Typography } from '@mui/material';
-import React, { useState } from 'react';
+import { Box, Grid, InputAdornment, Paper, Table, TableBody, TableCell, TableContainer, TableHead, TableRow, TextField, Typography } from '@mui/material';
+import React, { useContext, useEffect, useState } from 'react';
 import { TiArrowSortedDown, TiArrowSortedUp } from "react-icons/ti";
+import { FiSearch } from "react-icons/fi";
 import './TableInfo.css';
-
-function createData(name, calories, fat, carbs, protein) {
-    return { name, calories, fat, carbs, protein };
-}
-
-const rows = [
-    createData('Frozen yoghurt', 159, 6.0, 24, 4.0),
-    createData('Ice cream sandwich', 237, 9.0, 37, 4.3),
-    createData('Eclair', 262, 16.0, 24, 6.0),
-    createData('Cupcake', 305, 3.7, 67, 4.3),
-    createData('Gingerbread', 356, 16.0, 49, 3.9),
-];
+import { GlobalContext } from '../../App';
+import axios from 'axios';
 
 const TableInfo = () => {
-    const [data, setData] = useState(rows);
     const [order, setOrder] = useState('');
+    const [tableHeads, setTableHeads] = useState([]);
+    const [tableRows, setTableRows] = useState([]);
+    const [colValue, setColValue] = useState('');
+    const [searchValue, setSearchValue] = useState('');
+
+    useEffect(() => {
+        axios.get('http://localhost/api/list.php')
+            .then(res => {
+                if (res.data) {
+                    const result = res.data?.data;
+                    setTableHeads(result.headers);
+                    setTableRows(result.rows);
+                }
+            })
+            .catch(err => {
+                console.log(err.response);
+            })
+    }, []);
 
     const handleSorting = (col) => {
-        if(order === 'asc' || order === ''){
-            const sorted = [...data].sort((a, b) => 
-                a[col] > b[col]  ? 1 : -1
+        setColValue(col);
+
+        if (order === 'asc' || order === '') {
+            const sorted = [...tableRows].sort((a, b) =>
+                a[col] > b[col] ? 1 : -1
             );
-            setData(sorted)
+            setTableRows(sorted)
             setOrder('dsc');
-            // console.log(col);
-            console.log(sorted)
         }
-        else if(order === 'dsc'){
-            const sorted = [...data].sort((a, b) => 
-                a[col] <  b[col]  ? 1 : -1
+        else if (order === 'dsc') {
+            const sorted = [...tableRows].sort((a, b) =>
+                a[col] < b[col] ? 1 : -1
             );
-            setData(sorted)
+            setTableRows(sorted)
             setOrder('asc');
-            // console.log(col);
-            console.log(sorted);
         }
-    }
+    };
 
     return (
         <>
             <Box sx={{ m: 5 }}>
-                <Typography component="h3" sx={{ fontWeight: 500, fontSize: '26px', textAlign: 'center' , mb: 2}}>Table List</Typography>
+                <Typography component="h3" sx={{ fontWeight: 500, fontSize: '26px', textAlign: 'center', mb: 2 }}>Table List</Typography>
                 <Box>
+                    <Grid container spacing={3} className="search-inputs" sx={{ mb: 2 }}>
+                        {[1, 2, 3]?.map(item => {
+                            return (
+                                <Grid key={item} item xs={6} md={5} lg={3}>
+                                    <Typography variant="p" component="div" className="searchItem-title" >
+                                        Search by name
+                                    </Typography>
+                                    <TextField className="bg-white" onChange={((e) => setSearchValue(e.target.value))} fullWidth sx={{ padding: '1px 1px' }} InputProps={{
+                                        endAdornment: (
+                                            <InputAdornment position="end">
+                                                <FiSearch />
+                                            </InputAdornment>
+                                        )
+                                    }} />
+                                </Grid>
+                            )
+                        })}
+                    </Grid>
                     <TableContainer component={Paper} className="tableList" elevation={0}>
                         <Table sx={{ minWidth: 650 }} aria-label="simple table">
                             <TableHead>
-                                <TableRow >
-                                    <TableCell onClick={() => handleSorting('name')} className="pointer">
-                                        Dessert (100g serving) {order === 'asc' ? <TiArrowSortedDown /> : order === 'dsc' ? <TiArrowSortedUp /> : ''}
-                                    </TableCell>
-                                    <TableCell onClick={() => handleSorting('calories')} align="left" >Calories</TableCell>
-                                    <TableCell onClick={() => handleSorting('fat')} align="left">Fat&nbsp;(g)</TableCell>
-                                    <TableCell onClick={() => handleSorting('carbs')} align="left">Carbs&nbsp;(g)</TableCell>
-                                    <TableCell onClick={() => handleSorting('protein')} align="left">Protein&nbsp;(g)</TableCell>
+                                <TableRow>
+                                    {tableHeads?.map((item, index) => {
+                                        const { created_at, id, message, name } = item;
+
+                                        return (
+                                            <>
+                                                <TableCell onClick={() => handleSorting('id')} align="left" className="pointer">
+                                                    { id.title} {((order === 'asc') && (colValue === 'id')) ? <TiArrowSortedDown /> : ((order === 'dsc') && (colValue === 'id')) ? <TiArrowSortedUp /> : ''}
+                                                </TableCell>
+                                                <TableCell onClick={() => handleSorting('name')} align="left" className="pointer">
+                                                    {name.title} {((order === 'asc') && (colValue === 'name')) ? <TiArrowSortedDown /> : ((order === 'dsc') && (colValue === 'name')) ? <TiArrowSortedUp /> : ''}
+                                                </TableCell>
+                                                <TableCell onClick={() => handleSorting('message')} align="left" className="pointer">
+                                                    {message.title} {((order === 'asc') && (colValue === 'message')) ? <TiArrowSortedDown /> : ((order === 'dsc') && (colValue === 'message')) ? <TiArrowSortedUp /> : ''}
+                                                </TableCell>
+                                                <TableCell onClick={() => handleSorting('created_at')} align="left" className="pointer">
+                                                    {created_at.title} {((order === 'asc') && (colValue === 'created_at')) ? <TiArrowSortedDown /> : ((order === 'dsc') && (colValue === 'created_at')) ? <TiArrowSortedUp /> : ''}
+                                                </TableCell>
+                                            </>
+                                        )
+                                    })}
                                 </TableRow>
                             </TableHead>
                             <TableBody>
-                                {data?.map((row) => (
+                                {tableRows?.map((row) => (
                                     <TableRow
-                                        key={row.name}
+                                        key={row.id}
                                     >
                                         <TableCell component="th" scope="row">
-                                            {row.name}
+                                            {row?.id}
                                         </TableCell>
-                                        <TableCell align="left">{row.calories}</TableCell>
-                                        <TableCell align="left">{row.fat}</TableCell>
-                                        <TableCell align="left">{row.carbs}</TableCell>
-                                        <TableCell align="left">{row.protein}</TableCell>
+                                        <TableCell align="left">{row?.name}</TableCell>
+                                        <TableCell align="left">{row?.message}</TableCell>
+                                        <TableCell align="left">{row?.created_at}</TableCell>
                                     </TableRow>
                                 ))}
                             </TableBody>
