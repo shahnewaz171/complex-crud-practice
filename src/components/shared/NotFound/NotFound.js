9c6276ead2d6a5a021c8e86901131d8287f43152
shahnewaz171 <shahnewaz601@gmail.com>
2022-05-22 09:53:38 +0600

    designed not found page

--- src/components/shared/NotFound/NotFound.js
+++ src/components/shared/NotFound/NotFound.js
@@ -1,11 +1,41 @@
-import React from 'react';
+import { Box, Typography } from '@mui/material';
+import React, { useEffect } from 'react';
+import Logo from '../../../images/404.png';
+import { useNavigate } from 'react-router-dom';
 
 const NotFound = () => {
 
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        setTimeout(() => {
+            navigate(-1);
+        }, 3000)
+    }, [navigate]);
+
     return (
-        <div>
-            <h1>Not found</h1>
-        </div>
+        <>
+            <Box sx={{
+                bgcolor: "#fff",
+                borderRadius: "5px",
+                minHeight: "80vh"
+            }}>
+                <Box sx={{
+                    height: "60vh",
+                    display: "flex",
+                    justifyContent: "center",
+                }}>
+                    <Box
+                        component='img'
+                        src={Logo}
+                        sx={{ height: 1 }}
+                    />
+                </Box>
+                    <Typography variant='h5' component='h5' sx={{ fontWeight: "bold", textAlign: "center", my: 2 }}>
+                        Page not found!
+                    </Typography>
+            </Box>
+        </>
     );
 };
 
